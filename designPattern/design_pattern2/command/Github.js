d35aab9c11b34ea15d995af34cd055f6a16b09f6
Taemin Moon <tmmoond8@gmail.com>
2018-11-21 13:45:36 +0900

    design pattern #2 - command pattern

---
+++ designPattern/design_pattern2/command/Github.js
@@ -0,0 +1,20 @@
+const Github = class {
+  constructor(id, repo) {
+    this._base = `https://api.github.com/repos/${id}/${repo}/contents/`;
+  }
+  load(path) {
+    const id = `callback${Github._id++}`;
+    const f = Github[id] = ({ data: { content } }) => {
+      delete Github[id];
+      document.head.removeChild(s);
+      this._parser[0](content, ...this._parser[1]);
+    };
+    const s = document.createElement('script');
+    s.src = `${this._base + path}?callback=Github.${id}`;
+    document.head.appendChild(s);
+  }
+  setParser(f, ...arg) {
+    this._parser = [f, arg];
+  }
+}
+Github._id = 0;
