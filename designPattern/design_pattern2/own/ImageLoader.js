5c26e0acc895f74fe0a335d329150833fa321d63
Taemin Moon <tmmoond8@gmail.com>
2018-11-21 13:32:10 +0900

    design pattern #2 소유위임

--- designPattern/design_pattern2/own/ImageLoader.js
+++
@@ -1,9 +0,0 @@
-const ImageLoader = class extends Github {
-  constructor(id, repo, target) {
-    super(id, repo);
-    this._target = target;
-  }
-  _loaded(v) {
-    this._target.src = 'data:text/plain;base64,' + v;
-  }
-}
