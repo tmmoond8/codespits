7087de15e67de60cad3f76aa887726a6e0ad38e9
문태민 <tmmoon@synapsoft.co.kr>
2018-12-04 20:51:11 +0900

    code spitz design pattern #4 - Console Renderer

---
+++ designPattern/design_pattern4/base/ConsoleRenderer.js
@@ -0,0 +1,19 @@
+const TAB = '--';
+const ConsoleRenderer = class {
+  constructor() {}
+
+  render(data) {
+    const { task, list } = data;
+    console.log('---------------');
+    console.log(task.getTitle());
+    this._render(TAB, list);
+    console.log('---------------');
+  }
+  
+  _render(tab, list) {
+    list.forEach(item => {
+      console.log(tab + item.getTitle());
+      this._render(tab + TAB, item.getList());
+    })
+  }
+}
